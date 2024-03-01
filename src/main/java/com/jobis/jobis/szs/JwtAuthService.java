0f035e004061cbc23e779f4318044b0b95db2007
charlie <ddd1331@gmail.com>
2024-03-01 14:36:37 +0900

    AuthService UserService 분리

--- src/main/java/com/jobis/jobis/szs/JwtAuthService.java
+++ src/main/java/com/jobis/jobis/szs/JwtAuthService.java
@@ -3,19 +3,15 @@ package com.jobis.jobis.szs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class JwtAuthService implements AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
-    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
-    private final RegistrationValidator registrationValidator;
 
     public LoginResponse login(LoginRequest dto) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(dto.getUserId(), dto.getPassword());
@@ -32,32 +28,6 @@ public class JwtAuthService implements AuthService {
         return LoginResponse.builder().accessToken(token).build();
     }
 
-    public SignupResponse signup(SignupRequest dto) {
-
-        boolean allowed = registrationValidator.isAllowed(dto.getName(), dto.getRegNo());
-
-        if (!allowed) throw new UnauthorizedAccessException("가입이 불가능합니다");
-
-        Optional<User> existingUser = userRepository.findByUserId(dto.getUserId());
-        // TODO: custom exception
-        if (existingUser.isPresent()) throw new UnauthorizedAccessException("이미 가입된 아이디입니다");
-
-
-        User user = User.builder()
-                .userId(dto.getUserId())
-                .name((dto.getName()))
-                .regNo(passwordEncoder.encode(dto.getRegNo()))
-                .password(passwordEncoder.encode(dto.getPassword()))
-                .build();
-
-        userRepository.save(user);
-
-        String token = jwtService.generateToken(user);
-
-        return SignupResponse.builder().accessToken(token).build();
-
-
-    }
 
 
 }
--- src/main/java/com/jobis/jobis/szs/SzsController.java
+++ src/main/java/com/jobis/jobis/szs/SzsController.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.*;
 public class SzsController {
 
     private final JwtAuthService jwtAuthService;
+    private final UserService userService;
 
     @PostMapping("/signup")
     public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest dto) {
-        System.out.println("test");
-        System.out.println(dto);
-        SignupResponse res = jwtAuthService.signup(dto);
+        SignupResponse res = userService.signup(dto);
 
         return ResponseEntity.ok(res);
 
