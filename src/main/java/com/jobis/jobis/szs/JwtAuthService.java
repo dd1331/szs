af71992b603a1f156eb5aec8d8059fcb6bdb56cf
charlie <ddd1331@gmail.com>
2024-03-01 10:59:02 +0900

    회원가입 가능여부 확인기능 RegistrationValidator 인터페이스로 분리, MemoryRegistrationValidator로 구현

--- src/main/java/com/jobis/jobis/szs/JwtAuthService.java
+++ src/main/java/com/jobis/jobis/szs/JwtAuthService.java
@@ -6,14 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
-
-
-// TODO: 예외를 http로 변경필요
-// 허용되는건 디비말고 그냥 메모리에 올려버림 몇개 안돼서
 
 @Service
 @RequiredArgsConstructor
@@ -22,22 +15,7 @@ public class JwtAuthService implements AuthService {
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
-
-    private static final Map<String, String> allowedMembers = new HashMap<>();
-
-    static {
-        allowedMembers.put("동탁", "921108-1582816");
-        allowedMembers.put("관우", "681108-1582816");
-        allowedMembers.put("손권", "890601-2455116");
-        allowedMembers.put("유비", "790411-1656116");
-        allowedMembers.put("조조", "810326-2715702");
-    }
-
-    public boolean isMemberAllowed(String name, String id) {
-        String registeredId = allowedMembers.get(name);
-
-        return registeredId != null && registeredId.equals(id);
-    }
+    private final RegistrationValidator registrationValidator;
 
     public LoginResponse login(LoginRequest dto) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(dto.getUserId(), dto.getPassword());
@@ -50,7 +28,7 @@ public class JwtAuthService implements AuthService {
 
     public SignupResponse signup(SignupRequest dto) {
 
-        boolean allowed = isMemberAllowed(dto.getName(), dto.getRegNo());
+        boolean allowed = registrationValidator.isAllowed(dto.getName(), dto.getRegNo());
 
         if (!allowed) throw new UnauthorizedAccessException("가입이 불가능합니다");
 
@@ -59,12 +37,7 @@ public class JwtAuthService implements AuthService {
         if (existingUser.isPresent()) throw new UnauthorizedAccessException("이미 가입된 아이디입니다");
 
 
-        User user = User.builder()
-                .userId(dto.getUserId())
-                .name((dto.getName()))
-                .regNo(dto.getRegNo())
-                .password(passwordEncoder.encode(dto.getPassword()))
-                .build();
+        User user = User.builder().userId(dto.getUserId()).name((dto.getName())).regNo(dto.getRegNo()).password(passwordEncoder.encode(dto.getPassword())).build();
 
         userRepository.save(user);
 
@@ -74,8 +47,6 @@ public class JwtAuthService implements AuthService {
 
 
     }
-
-
 
 
 }
