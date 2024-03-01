096822d86ffef047b0217ded9af265c5608d0d5b
charlie <ddd1331@gmail.com>
2024-03-01 10:21:52 +0900

    산출세액 계산기

--- src/main/java/com/jobis/jobis/szs/ScrapServiceImpl.java
+++ src/main/java/com/jobis/jobis/szs/ScrapServiceImpl.java
@@ -19,12 +19,13 @@ import java.util.List;
 import java.util.Map;
 
 @Data
- class DeductionData {
+class DeductionData {
     private String status;
     private DeductionDetail data;
     private Errors errors;
 
 }
+
 @Data
 class DeductionDetail {
     private int 종합소득금액;
@@ -32,6 +33,7 @@ class DeductionDetail {
     private IncomeDeduction 소득공제;
 
 }
+
 @Data
 class IncomeDeduction {
     private List<NationalPensionDeduction> 국민연금;
@@ -39,18 +41,21 @@ class IncomeDeduction {
     private String 세액공제;
 
 }
+
 @Data
 class NationalPensionDeduction {
     private String 월;
     private String 공제액;
 
 }
+
 @Data
 class CreditCardDeduction {
     private List<Map<String, String>> month;
     private int year;
 
 }
+
 @Data
 class Errors {
     private String code;
@@ -60,16 +65,15 @@ class Errors {
 }
 
 
-
 @RequiredArgsConstructor
 @Service
-public class ScrapServiceImpl implements  ScrapService {
+public class ScrapServiceImpl implements ScrapService {
 
     public void format(DeductionData deductionData) {
 
 
         List<NationalPensionDeduction> national = deductionData.getData().get소득공제().get국민연금();
-        System.out.println("deductionData "+national);
+        System.out.println("deductionData " + national);
 
         String 세액공제 = deductionData.getData().get소득공제().get세액공제();
 
@@ -77,75 +81,49 @@ public class ScrapServiceImpl implements  ScrapService {
         // TODO: check type
         int 종합소득 = deductionData.getData().get종합소득금액();
 
-        BigDecimal totalNational =  national.stream() // TODO: check type
-                .map(NationalPensionDeduction::get공제액)
-                .map(공제액 -> new BigDecimal(공제액.replaceAll(",", "")))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal totalNational = national.stream() // TODO: check type
+                .map(NationalPensionDeduction::get공제액).map(공제액 -> new BigDecimal(공제액.replaceAll(",", ""))).reduce(BigDecimal.ZERO, BigDecimal::add);
 
         List<Map<String, String>> credit = deductionData.getData().get소득공제().get신용카드소득공제().getMonth();
 
 
-
-        BigDecimal totalCredit = credit.stream()
-                .flatMap(map -> map.values().stream())
-                .map(value -> new BigDecimal(value.replaceAll(",", "")))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal totalCredit = credit.stream().flatMap(map -> map.values().stream()).map(value -> new BigDecimal(value.replaceAll(",", ""))).reduce(BigDecimal.ZERO, BigDecimal::add);
 
         BigDecimal 총소득공제 = totalCredit.add(totalNational);
         BigDecimal 과세표준 = (new BigDecimal(종합소득).subtract(총소득공제));
 
 
         // TODO: 숫자 타입 확인
-        System.out.println("totalCredit "+totalCredit );
-        System.out.println("totalNational "+totalNational );
-        System.out.println("세액공제 "+세액공제 );
-        System.out.println("종합소득 "+종합소득 );
-        System.out.println("과세표준 "+과세표준);
+        System.out.println("totalCredit " + totalCredit);
+        System.out.println("totalNational " + totalNational);
+        System.out.println("세액공제 " + 세액공제);
+        System.out.println("종합소득 " + 종합소득);
+        System.out.println("과세표준 " + 과세표준);
     }
 
 
-
-
-    public void scrap(){
+    public void scrap() {
         String url = "https://codetest-v4.3o3.co.kr";
         ScrapRequest requestBody = new ScrapRequest("동탁", "921108-1582816");
-        HttpClient httpClient = HttpClient.create()
-                .headers(httpHeaders -> httpHeaders.set("X-API-KEY", "oKJg+BDBIjKkayD9DgNHNQ=="))
-                .secure(sslContextSpec -> sslContextSpec.sslContext(
-                        SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE)
-                ));
+        HttpClient httpClient = HttpClient.create().headers(httpHeaders -> httpHeaders.set("X-API-KEY", "oKJg+BDBIjKkayD9DgNHNQ==")).secure(sslContextSpec -> sslContextSpec.sslContext(SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE)));
 
         ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
-        WebClient webClient = WebClient.builder()
-                .baseUrl(url)
-                .defaultHeader("Content-Type", "application/json")
-                .clientConnector(connector)
-                .build();
+        WebClient webClient = WebClient.builder().baseUrl(url).defaultHeader("Content-Type", "application/json").clientConnector(connector).build();
 
-        Mono<DeductionData> responseMono = webClient.method(HttpMethod.POST)
-                .uri("/scrap")
-                .body(BodyInserters.fromValue(requestBody))
-                .retrieve()
+        Mono<DeductionData> responseMono = webClient.method(HttpMethod.POST).uri("/scrap").body(BodyInserters.fromValue(requestBody)).retrieve()
 //                .onStatus(HttpStatus::isError, response -> Mono.error(new RuntimeException("HTTP error")))
                 .bodyToMono(DeductionData.class);
 
-        responseMono.subscribe(
-                response -> {
+        responseMono.subscribe(response -> {
 
-                    System.out.println("응답 받음: " + response);
-                    format(response);
-                },
-                error -> {
-                    System.err.println("에러 발생: " + error.getMessage());
-                }
-        );
-
-
+            System.out.println("응답 받음: " + response);
+            format(response);
+        }, error -> {
+            System.err.println("에러 발생: " + error.getMessage());
+        });
 
 
     }
-
-
 
 
 }
---
+++ src/test/java/com/jobis/jobis/szs/TaxCalculatorTest.java
@@ -0,0 +1,29 @@
+package com.jobis.jobis.szs;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+public class TaxCalculatorTest {
+
+    @Test
+    public void testCalculateTax() {
+        testCalculateTax("1,400,000", "84,000");
+        testCalculateTax("20,000,000", "1,740,000");
+        testCalculateTax("50,000,000", "6,240,000");
+        testCalculateTax("88,000,000", "15,360,000");
+        testCalculateTax("150,000,000", "37,060,000");
+        testCalculateTax("300,000,000", "94,060,000");
+        testCalculateTax("500,000,000", "174,060,000");
+        testCalculateTax("500,000,001", "174,060,000");
+        testCalculateTax("1,000,000,000", "384,060,000");
+        testCalculateTax("1,000,000,001", "384,060,000");
+        testCalculateTax("1,500,000,000", "609,060,000");
+
+    }
+
+    private void testCalculateTax(String taxableIncomeStr, String expectedTaxStr) {
+        BigDecimal taxableIncome = new BigDecimal(taxableIncomeStr.replaceAll(",", ""));
+        BigDecimal expectedTax = new BigDecimal(expectedTaxStr.replaceAll(",", ""));
+        BigDecimal actualTax = TaxCalculator.calculateTax(taxableIncome);
+        assertEquals(expectedTax, actualTax);
+    }
+}
